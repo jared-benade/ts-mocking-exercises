6849254c66942b1d9671b9b496f97687fd3e0e6e
Jared Benade <jared.benade@chillisoft.co.za>
2021-04-30 15:45:50 +0200

    WIP: Implement Jest tests for scenario 5

--- src/tests-to-implement/05_fake_timers.spec.ts
+++ src/tests-to-implement/05_fake_timers.spec.ts
@@ -2,10 +2,24 @@ import { describe, expect, it } from '@jest/globals'
 import { generateDayMessage } from '../tests-to-implement/05_fake_timers'
 
 describe('generateDayMessage', () => {
-  it.skip('returns a message containing the current time', () => {
+  beforeEach(() => {
+    jest.useFakeTimers('modern');
+  })
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it('returns a message containing the current time', () => {
     // Arrange
+    const currentDate = new Date('2021-04-30T11:01:58.135+02:00');
+    const expected = '[11:01:58 PM]: Today is Friday'
+
+    jest.setSystemTime(currentDate)
     // Act
+    const result = generateDayMessage()
     // Assert
+    expect(result).toEqual(expected)
   })
 
   it.skip('returns a message containing the current time after some time has elapsed', () => {
